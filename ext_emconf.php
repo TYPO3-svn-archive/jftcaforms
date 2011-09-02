2ae8e1ba40a52880ac9c3d9b5be2c2eb7ed69089
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-09-02 16:11:21 +0000

    Security fix, please upgrade
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jftcaforms/trunk@51637 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "jftcaforms".
 #
-# Auto generated 30-08-2011 23:19
+# Auto generated 02-09-2011 18:11
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.2.0',
+	'version' => '0.2.1',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:41:{s:12:"ext_icon.gif";s:4:"dd0d";s:17:"ext_localconf.php";s:4:"a0fd";s:14:"ext_tables.php";s:4:"a0fd";s:16:"locallang_db.xml";s:4:"958e";s:14:"doc/manual.sxw";s:4:"f52b";s:35:"lib/class.tx_jftcaforms_tceFunc.php";s:4:"fc6b";s:35:"res/colorpicker/css/colorpicker.css";s:4:"4b91";s:32:"res/colorpicker/images/blank.gif";s:4:"5639";s:49:"res/colorpicker/images/colorpicker_background.png";s:4:"a79f";s:44:"res/colorpicker/images/colorpicker_extra.png";s:4:"920c";s:55:"res/colorpicker/images/colorpicker_extra_background.png";s:4:"ae83";s:42:"res/colorpicker/images/colorpicker_hex.png";s:4:"16d6";s:44:"res/colorpicker/images/colorpicker_hsb_b.png";s:4:"2be4";s:44:"res/colorpicker/images/colorpicker_hsb_h.png";s:4:"d474";s:44:"res/colorpicker/images/colorpicker_hsb_s.png";s:4:"5ff5";s:44:"res/colorpicker/images/colorpicker_indic.gif";s:4:"f485";s:46:"res/colorpicker/images/colorpicker_overlay.png";s:4:"c7a3";s:44:"res/colorpicker/images/colorpicker_rgb_b.png";s:4:"2be4";s:44:"res/colorpicker/images/colorpicker_rgb_g.png";s:4:"dc17";s:44:"res/colorpicker/images/colorpicker_rgb_r.png";s:4:"87ee";s:45:"res/colorpicker/images/colorpicker_select.gif";s:4:"cec4";s:45:"res/colorpicker/images/colorpicker_submit.png";s:4:"e887";s:44:"res/colorpicker/images/custom_background.png";s:4:"06c9";s:37:"res/colorpicker/images/custom_hex.png";s:4:"e27a";s:39:"res/colorpicker/images/custom_hsb_b.png";s:4:"b455";s:39:"res/colorpicker/images/custom_hsb_h.png";s:4:"4a1a";s:39:"res/colorpicker/images/custom_hsb_s.png";s:4:"83aa";s:39:"res/colorpicker/images/custom_indic.gif";s:4:"0466";s:39:"res/colorpicker/images/custom_rgb_b.png";s:4:"f6e5";s:39:"res/colorpicker/images/custom_rgb_g.png";s:4:"b1e2";s:39:"res/colorpicker/images/custom_rgb_r.png";s:4:"8ae4";s:40:"res/colorpicker/images/custom_submit.png";s:4:"3542";s:33:"res/colorpicker/images/select.png";s:4:"7b08";s:34:"res/colorpicker/images/select2.png";s:4:"c8d1";s:33:"res/colorpicker/images/slider.png";s:4:"3a50";s:33:"res/colorpicker/js/colorpicker.js";s:4:"86ce";s:23:"res/extjs/ux/Spinner.js";s:4:"e3c6";s:28:"res/extjs/ux/SpinnerField.js";s:4:"0c44";s:28:"res/extjs/ux/css/Spinner.css";s:4:"119e";s:37:"res/extjs/ux/images/spinner-split.gif";s:4:"d228";s:31:"res/extjs/ux/images/spinner.gif";s:4:"7732";}',
+	'_md5_values_when_last_written' => 'a:41:{s:12:"ext_icon.gif";s:4:"dd0d";s:17:"ext_localconf.php";s:4:"a0fd";s:14:"ext_tables.php";s:4:"a0fd";s:16:"locallang_db.xml";s:4:"958e";s:14:"doc/manual.sxw";s:4:"6dde";s:35:"lib/class.tx_jftcaforms_tceFunc.php";s:4:"5b57";s:35:"res/colorpicker/css/colorpicker.css";s:4:"4b91";s:32:"res/colorpicker/images/blank.gif";s:4:"5639";s:49:"res/colorpicker/images/colorpicker_background.png";s:4:"a79f";s:44:"res/colorpicker/images/colorpicker_extra.png";s:4:"920c";s:55:"res/colorpicker/images/colorpicker_extra_background.png";s:4:"ae83";s:42:"res/colorpicker/images/colorpicker_hex.png";s:4:"16d6";s:44:"res/colorpicker/images/colorpicker_hsb_b.png";s:4:"2be4";s:44:"res/colorpicker/images/colorpicker_hsb_h.png";s:4:"d474";s:44:"res/colorpicker/images/colorpicker_hsb_s.png";s:4:"5ff5";s:44:"res/colorpicker/images/colorpicker_indic.gif";s:4:"f485";s:46:"res/colorpicker/images/colorpicker_overlay.png";s:4:"c7a3";s:44:"res/colorpicker/images/colorpicker_rgb_b.png";s:4:"2be4";s:44:"res/colorpicker/images/colorpicker_rgb_g.png";s:4:"dc17";s:44:"res/colorpicker/images/colorpicker_rgb_r.png";s:4:"87ee";s:45:"res/colorpicker/images/colorpicker_select.gif";s:4:"cec4";s:45:"res/colorpicker/images/colorpicker_submit.png";s:4:"e887";s:44:"res/colorpicker/images/custom_background.png";s:4:"06c9";s:37:"res/colorpicker/images/custom_hex.png";s:4:"e27a";s:39:"res/colorpicker/images/custom_hsb_b.png";s:4:"b455";s:39:"res/colorpicker/images/custom_hsb_h.png";s:4:"4a1a";s:39:"res/colorpicker/images/custom_hsb_s.png";s:4:"83aa";s:39:"res/colorpicker/images/custom_indic.gif";s:4:"0466";s:39:"res/colorpicker/images/custom_rgb_b.png";s:4:"f6e5";s:39:"res/colorpicker/images/custom_rgb_g.png";s:4:"b1e2";s:39:"res/colorpicker/images/custom_rgb_r.png";s:4:"8ae4";s:40:"res/colorpicker/images/custom_submit.png";s:4:"3542";s:33:"res/colorpicker/images/select.png";s:4:"7b08";s:34:"res/colorpicker/images/select2.png";s:4:"c8d1";s:33:"res/colorpicker/images/slider.png";s:4:"3a50";s:33:"res/colorpicker/js/colorpicker.js";s:4:"86ce";s:23:"res/extjs/ux/Spinner.js";s:4:"e3c6";s:28:"res/extjs/ux/SpinnerField.js";s:4:"0c44";s:28:"res/extjs/ux/css/Spinner.css";s:4:"119e";s:37:"res/extjs/ux/images/spinner-split.gif";s:4:"d228";s:31:"res/extjs/ux/images/spinner.gif";s:4:"7732";}',
 	'suggests' => array(
 	),
 );
